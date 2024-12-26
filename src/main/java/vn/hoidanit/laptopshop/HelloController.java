caa98e5aa8cf6fa9f8d91b096a666f017a5c1034
Manh Phan <imseirousmanh@gmail.com>
2024-12-26 13:06:07 +0700

    Commit 2rd at #36

--- src/main/java/vn/hoidanit/laptopshop/HelloController.java
+++ src/main/java/vn/hoidanit/laptopshop/HelloController.java
@@ -6,9 +6,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    @GetMapping("/")
+	@GetMapping("/")
 	public String index() {
-		return "Hello World from Spring Boot! to ManhFangg";
+		return "Hello World from Spring Boot! from Manh 3rd";
+	}
+
+	@GetMapping("/user")
+	public String userPage() {
+		return "User can access this page";
+	}
+
+	@GetMapping("/admin")
+	public String admin() {
+		return "Only user can access this page";
 	}
 
 }
