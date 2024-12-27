427dea08bb9681a69254fd784e2eb9074376acc8
Manh Phan <imseirousmanh@gmail.com>
2024-12-27 08:53:38 +0700

    add static to resources which symbol for View in Model MVC

--- src/main/java/vn/hoidanit/laptopshop/controller/userController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/userController.java
@@ -1,5 +1,7 @@
 package vn.hoidanit.laptopshop.controller;
-// this file is the primary part of Model MVC spring: main function is charge for redirecting your website
+// this file is the primary part of Model MVC spring: main function is charge
+
+// for redirecting your website
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,26 +9,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import vn.hoidanit.laptopshop.service.UserService;
 
-// @Controller
-// public class userController {                Model MVC code
-// @RequestMapping("/")
-// public String getHomePage() {
-
-// return "hello from controller";
-// }
-// }
-
-@RestController // Model Restful API
-public class userController {
-
-    private UserService userService;
-
-    public userController(UserService userService) {
-        this.userService = userService;
-    }
-
-    @GetMapping("")
+@Controller
+public class userController { // Model MVC code
+    @RequestMapping("/")
     public String getHomePage() {
-        return this.userService.handleHello();
+
+        return "fangg";
     }
 }
+
+// @RestController // Model Restful API
+// public class userController {
+
+// private UserService userService;
+
+// public userController(UserService userService) {
+// this.userService = userService;
+// }
+
+// @GetMapping("")
+// public String getHomePage() {
+// return this.userService.handleHello();
+// }
+// }
