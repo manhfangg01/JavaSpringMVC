20cb68e85b4d39e060de52293f9ccf98589f531c
Manh Phan <imseirousmanh@gmail.com>
2024-12-27 08:30:53 +0700

    build controller and its service to create a Model and apply DI for controller model restful API, unbuildable MVC due to uncontruct view

---
+++ src/main/java/vn/hoidanit/laptopshop/controller/userController.java
@@ -0,0 +1,32 @@
+package vn.hoidanit.laptopshop.controller;
+// this file is the primary part of Model MVC spring: main function is charge for redirecting your website
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import vn.hoidanit.laptopshop.service.UserService;
+
+// @Controller
+// public class userController {                Model MVC code
+// @RequestMapping("/")
+// public String getHomePage() {
+
+// return "hello from controller";
+// }
+// }
+
+@RestController // Model Restful API
+public class userController {
+
+    private UserService userService;
+
+    public userController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("")
+    public String getHomePage() {
+        return this.userService.handleHello();
+    }
+}
